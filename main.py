2b7830fb91280266b03eb38280856802c225c9f8
ameen-91 <mohammedameen9011@gmail.com>
2024-12-29 06:01:21 +0530

    fix metadata

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def metadata():
     return {
         "Debug": DEBUG,
         "Frontend_Origins": FRONTEND_ORIGINS,
-        "OPENAI_API_Key_Set": bool(config.get("API_KEY")),
+        "OPENAI_API_Key_Set": bool(config.get("OPENAI_API_KEY")),
         "Model_Name": config.get("MODEL_NAME"),
         "Base_URL": config.get("BASE_URL"),
         "YOUTUBE_API_Key_Set": bool(config.get("YOUTUBE_API_KEY")),

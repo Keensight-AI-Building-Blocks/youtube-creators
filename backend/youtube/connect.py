9d58460426039e10af2aa7eacc4d59264982f88d
ameen-91 <mohammedameen9011@gmail.com>
2024-12-29 05:52:26 +0530

    default vals

--- backend/youtube/connect.py
+++ backend/youtube/connect.py
@@ -2,6 +2,6 @@ from googleapiclient.discovery import build
 from backend.config import config
 
 
-api_key = config.get("YOUTUBE_API_KEY")
+api_key = config.get("YOUTUBE_API_KEY", default="abc123")
 
 service = build('youtube', 'v3', developerKey=api_key)

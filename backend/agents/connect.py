2a726a3e29c89469ebe6d41b6fc24525c38bce28
ameen-91 <mohammedameen9011@gmail.com>
2024-12-29 05:45:23 +0530

    api fix

--- backend/agents/connect.py
+++ backend/agents/connect.py
@@ -3,7 +3,8 @@ from backend.config import config
 
 
 MODEL_NAME = config.get("MODEL_NAME", cast=str, default="gpt-4o-mini")
-OPENAI_API_KEY = config.get("OPENAI_API_KEY", cast=str, default="")
+OPENAI_API_KEY = config.get("OPENAI_API_KEY", cast=str, default="abc123")
 BASE_URL = config.get("BASE_URL", cast=str, default="https://api.openai.com/v1/")
+
 
 model = ChatOpenAI(model=MODEL_NAME, api_key=OPENAI_API_KEY, base_url=BASE_URL)
